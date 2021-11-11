d237a66f7e9cde632cf5b8c2e45945408e60884c
souljavan <jcavanilles@gmail.com>
2021-11-11 01:36:03 +0100

    iteracion 22

---
+++ iteracion22.js
@@ -0,0 +1,7 @@
+const getCharacters = async () => {
+    await fetch('https://rickandmortyapi.com/api/character')
+    .then(res => res.json())
+    .then(characters => console.log(characters));
+}
+
+getCharacters();
